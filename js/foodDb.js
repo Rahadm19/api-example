d82cf62d8c6b98a254a312768c46dc29a089e4d6
Rahadm19 <rahadm19@gmail.comm>
2022-02-26 20:48:26 +0600

    This is the most part of api

--- js/foodDb.js
+++ js/foodDb.js
@@ -1,7 +1,7 @@
 const searchFood = () =>{
   const searchField = document.getElementById('input-search');
   const searchText = searchField.value ;
-  console.log(searchText);
+  // console.log(searchText);
   searchField.value = "";
   const  url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
   fetch(url)
@@ -10,6 +10,7 @@ const searchFood = () =>{
 }
 const displaySearchResult = meals =>{
   const searchResult =  document.getElementById('search-result');
+  searchResult.textContent= "";
   
   // for(const meal of meals){
   //   console.log(meal);
@@ -18,19 +19,42 @@ const displaySearchResult = meals =>{
   // for the same for upper and downer 
 
   meals.forEach (meal =>{
-    console.log(meal);
+    // console.log(meal);
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
-    <div class="card h-100">
+    <div onclick = "loadMealDetails(${meal.idMeal})" class="card h-100">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
               <h2 class="card-title">${meal.strMeal}</h2>
               <p class="card-text">Meal Area:${meal.strArea}</p>
-              <p>Catagory : ${meal.strCategory}</p>
+              <p>Catagory : ${meal.strInstructions.slice(0,200)}</p>
             </div>
           </div>
     `;
     searchResult.appendChild(div);
   })
+}
+
+const loadMealDetails = mealId =>{
+  // console.log(mealId);
+   const url = `
+          https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}
+  `;
+  fetch(url)
+    .then(res => res.json())
+    .then ( data =>displayMealDetails(data.meals[0]))
+}
+const displayMealDetails = meal =>{
+  const detailsField = document.getElementById ('details-field');
+  detailsField.textContent = '';
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h1 class="card-title">${meal.strMeal}</h1>
+      <p class="card-text">Details :  ${meal.strInstructions.slice(0,200)}</p>
+      
+  `;
+  detailsField.appendChild(div);
 }
